b4c7986970975b15cdbb45af0dd1b24bcd0ac5e8
bslee <beomsu317@gmail.com>
2020-06-05 14:48:26 +0900

    memory_dump created

---
+++ Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+ARCH := arm64-v8a
+LOCAL_CFLAGS += -fPIE -pie
+LOCAL_MODULE := memory_dump
+LOCAL_SRC_FILES := memory_dump.c
+include $(BUILD_EXECUTABLE)
---
+++ memory_dump.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#include <errno.h>
+#include <linux/uio.h>
+#include <elf.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+#define MEDIUM_SIZE 1024
+#define SMALL_SIZE 256
+
+int pid;
+char pname[SMALL_SIZE];
+
+void error(char *msg){
+	perror(msg);
+	exit(-1);
+}
+
+void dump(unsigned long long start,unsigned long long end){
+	unsigned char data;
+	char dump_name[SMALL_SIZE];
+	struct user_pt_regs regs;
+	struct iovec io;
+	long long size = 0;
+	FILE *fp;
+	io.iov_base = &regs;
+	io.iov_len = sizeof(regs);
+	
+	// // Get Register
+	// if(ptrace(PTRACE_GETREGSET,pid, (void*)NT_PRSTATUS, &io) < 0){
+	// 	error("ptrace() PTRACE_GETREGSET error");
+	// }
+
+	// printf("%p\n",regs.sp);
+	size = end - start;
+	if(size <=0 ) error("[-] size error");
+	char *p = malloc(size);
+	printf("[+] size: 0x%llx\n",size);
+	if(ptrace(PTRACE_ATTACH,pid,0,0) < 0) error("[-] ptrace() PTRACE_ATTACH error");
+	for(long long i = 0; i < size; i++,p++){
+		if(i%0x1000 == 0) printf("  [+] dumping 0x%llx\n",i);
+		if ((data = ptrace(PTRACE_PEEKDATA,pid,start++,0)) < 0) error("[-] ptrace() PEEKDATA error");
+			*p = data;
+		
+	}
+	if(ptrace(PTRACE_DETACH, pid, 0, 0) < 0) error("[-] ptrace() PTRACE_DETACH error");
+	p = p - size;
+
+	sprintf(dump_name,"./%s_%llx_%llx",pname,start,end);
+
+	if((fp = fopen(dump_name,"wb")) < 0) error("[-] fopen() error");
+	fwrite(p,1,size,fp);
+	fclose(fp);
+	free(p);
+
+}
+
+int ps_exists_check(int pid){
+	char cmdline[MEDIUM_SIZE];
+	struct dirent *entry;
+	struct stat file_stat;
+	int dir_pid;
+	DIR *dir;
+	FILE *fp;
+	char *ptr;
+	
+	dir = opendir("/proc");
+	
+	while((entry = readdir(dir))!=NULL){
+		
+		lstat(entry->d_name,&file_stat);
+		if(!S_ISDIR(file_stat.st_mode)) {
+			continue;
+		}
+		dir_pid = atoi(entry->d_name);
+		if(dir_pid == pid){
+			sprintf(cmdline,"/proc/%d/status",pid);
+			
+			if((fp = fopen(cmdline,"rt")) < 0) error("fopen() error");
+			fgets(pname,0x100,fp);
+			fclose(fp);
+
+			// printf("%s\n",pname);
+			ptr = strtok(pname,"\t");
+			ptr = strtok(NULL,"\t");
+			strcpy(pname,ptr);
+			pname[strlen(pname)-1]='\0';
+			printf("[+] Process name: %s\n",pname);
+			
+			return 1;
+		}
+	}
+	return -1;
+}
+
+int main(int argc,char* argv[],char* env[]) {
+	
+	FILE *fp;
+	struct dirent *entry;
+	char* ptr;
+	unsigned long long start,end;
+	
+	if(argc != 4){
+		printf("[+] Usage: %s [pid] [start] [end]\n",argv[0]);
+		exit(0);
+	}
+
+	pid = atoi(argv[1]);
+	start = strtoull(argv[2],NULL,16);
+	end = strtoull(argv[3],NULL,16);
+	
+	if(ps_exists_check(pid) < 0){
+		error("[-] Unknown Process\n");
+	}
+
+	printf("[+] pid:  %d\n",pid);
+	printf("[+] start: %llx\n[+] end: %llx\n",start,end);
+	
+	dump(start,end);
+
+	printf("[+] Done!\n");
+
+	return 0;
+}
+
