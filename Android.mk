654ffa2c11e285d34552d9dd726f1dcc8bfb358c
beomsu317 <38850856+beomsu317@users.noreply.github.com>
2020-06-17 11:13:29 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 APP_ABI := arm64-v8a
 LOCAL_CFLAGS += -fPIE -pie
-LOCAL_MODULE := memory_dump
-LOCAL_SRC_FILES := memory_dump.c
+LOCAL_MODULE := memdump
+LOCAL_SRC_FILES := memdump.c
 include $(BUILD_EXECUTABLE)
